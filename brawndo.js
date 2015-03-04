6ef0ebe05fb05bd5e2a5fe6223dd3eaaa61e7782
Algis Woss <awoss@dropoff.com>
2015-03-04 13:47:18 -0600

    0001
    * Initial commit

---
+++ brawndo.js
@@ -0,0 +1,226 @@
+'use strict';
+/**
+ * Created by alwoss on 2/27/15.
+ */
+var _       = require('lodash');
+var request = require('superagent');
+
+var signing = require('./signing');
+
+var configured = false;
+
+var public_key = void(0);
+var private_key = void(0);
+var hasher_url = void(0);
+
+var API_INFO_PATH = '/info';
+var API_ESTIMATE_PATH = '/estimate';
+var API_ORDER_PATH = '/order';
+
+var API_INFO_URL = void(0);
+var API_ESTIMATE_URL = void(0);
+var API_ORDER_URL = void(0);
+
+var signing_mw = function(path, callback) {
+  return function(request) {
+    var timestamp = signing.generateXDropoffDate().x_dropoff_date;
+
+    request.set('X-Dropoff-Date', timestamp);
+
+    var headers = request._header;
+    if ((!headers || _.size(headers) === 0) && request.header && _.size(request.header) > 0) {
+      headers = request.header;
+    } else if (!headers || _.size(headers) === 0) {
+      headers = {};
+      headers['x-dropoff-date'] = request.get('X-Dropoff-Date');
+      headers.accept = request.get('Accept');
+    }
+
+    var params = {
+      method : request.method,
+      return_as_auth_header : true,
+      public_key : public_key,
+      private_key : private_key,
+      hasher_url : hasher_url,
+      headers : headers,
+      signed_headers : _.keys(headers),
+      path : path
+    };
+
+    if (typeof navigator !== 'undefined' && navigator.userAgent) {
+      headers['user-agent'] = navigator.userAgent;
+      params.signed_headers.push('user-agent');
+    } else if (request.get('user-agent')) {
+      headers['user-agent'] = request.get('user-agent');
+      params.signed_headers.push('user-agent');
+    }
+
+    if (typeof document !== 'undefined' && document.location && document.location.host) {
+      headers.host = document.location.host;
+      params.signed_headers.push('host');
+    } else if (request.get('user-agent')) {
+      headers.host = request.get('host');
+      params.signed_headers.push('host');
+    }
+
+    if (typeof document !== 'undefined' && document.location && document.location.href) {
+      headers.referer = document.location.href;
+      params.signed_headers.push('referer');
+    }
+
+    if (callback) {
+      signing.sign(params, function(error, data) {
+        if (error) {
+          callback(error);
+        } else {
+          request.set('Authorization', data);
+          request.end(callback);
+        }
+      });
+    }
+
+    return request;
+  };
+};
+
+module.exports.createHash = function(params) {
+  return signing.createHash(params);
+};
+
+module.exports.getConfiguration = function() {
+  return {
+    public_key : public_key,
+    private_key : private_key,
+    hasher_url  : hasher_url
+  };
+};
+
+module.exports.configure = function(params) {
+  if (!params.public_key || !(params.private_key || params.hasher_url) || !params.api_url) {
+    throw new Error('Set the public key, private key or hasher_url, and api url');
+  }
+  public_key  = params.public_key;
+  private_key = params.private_key;
+  hasher_url  = params.hasher_url;
+  var api_url = params.api_url;
+
+  if (api_url.indexOf('/') === (api_url.length - 1)) {
+    api_url = api_url.substring(0, (api_url.length-1));
+  }
+  API_INFO_URL = api_url + API_INFO_PATH;
+  API_ESTIMATE_URL = api_url + API_ESTIMATE_PATH;
+  API_ORDER_URL = api_url + API_ORDER_PATH;
+  configured = true;
+};
+
+module.exports.info = function(callback) {
+  if (!configured) {
+    throw new Error('Call configure before calling the api');
+  }
+  request
+    .get(API_INFO_URL)
+    .set('Accept', 'application/json')
+    .use(signing_mw(API_INFO_PATH, function(error, response){
+      if (error) {
+        callback(error);
+      } else if (response.status === 200 && response.body) {
+        callback(void(0), response.body);
+      } else {
+        callback(new Error('response.status is ' + response.status), response.body);
+      }
+    }));
+};
+
+module.exports.getEstimate = function(params, callback) {
+  if (!configured) {
+    throw new Error('Call configure before calling the api');
+  }
+
+  if (!params.pickup || !params.destination) {
+    throw new Error('Call requires pickup and destination query parameters');
+  }
+
+  request
+    .get(API_ESTIMATE_URL)
+    .set('Accept', 'application/json')
+    .query(params)
+    .use(signing_mw(API_ESTIMATE_PATH, function(error, response){
+      if (error) {
+        callback(error);
+      } else if (response.status === 200 && response.body) {
+        callback(void(0), response.body);
+      } else {
+        callback(new Error('response.status is ' + response.status), response.body);
+      }
+    }));
+};
+
+module.exports.submitOrder = function(params, callback) {
+  if (!configured) {
+    throw new Error('Call configure before calling the api');
+  }
+  request
+    .post(API_ORDER_URL)
+    .set('Accept', 'application/json')
+    .send(params)
+    .use(signing_mw(API_ORDER_PATH))
+    .end(function(error, response){
+      if (error) {
+        callback(error);
+      } else if (response.status === 200 && response.body) {
+        callback(void(0), response.body);
+      } else {
+        callback(new Error('response.status is ' + response.status), response.body);
+      }
+    });
+};
+
+module.exports.getOrder = function(order_id, callback) {
+  if (!order_id) {
+    throw new Error('Call requires pickup and destination query parameters');
+  }
+
+  request
+    .get(API_ORDER_URL + '/' + order_id)
+    .set('Accept', 'application/json')
+    .use(signing_mw(API_ORDER_PATH, function(error, response) {
+      if (error) {
+        callback(error);
+      } else if (response.status === 200 && response.body) {
+        callback(void(0), response.body);
+      } else {
+        callback(new Error('response.status is ' + response.status), response.body);
+      }
+    }));
+};
+
+module.exports.getOrders = function(callback) {
+  request
+    .get(API_ORDER_URL)
+    .set('Accept', 'application/json')
+    .use(signing_mw(API_ORDER_PATH, function (error, response) {
+      if (error) {
+        callback(error);
+      } else if (response.status === 200 && response.body) {
+        callback(void(0), response.body);
+      } else {
+        callback(new Error('response.status is ' + response.status), response.body);
+      }
+    }));
+};
+
+module.exports.getOrdersFrom = function(last_key, callback) {
+  request
+    .get(API_ORDER_URL)
+    .query({ last_key : last_key })
+    .set('Accept', 'application/json')
+    .use(signing_mw(API_ORDER_PATH, function(error, response){
+      if (error) {
+        callback(error);
+      } else if (response.status === 200 && response.body) {
+        callback(void(0), response.body);
+      } else {
+        callback(new Error('response.status is ' + response.status), response.body);
+      }
+    }));
+};
---
+++ signing.js
@@ -0,0 +1,205 @@
+'use strict';
+/**
+ * Created by alwoss on 2/27/15.
+ */
+var _             = require('lodash');
+var async         = require('async');
+var moment        = require('moment');
+var request       = require('superagent');
+var CryptoJS      = { enc : {} };
+
+CryptoJS.HmacSHA512  = require('crypto-js/hmac-sha512');
+CryptoJS.enc.Hex     = require('crypto-js/enc-hex');
+
+var NO_ERROR  = void(0);
+var DONE      = 'DONE';
+var NOOP      = 'NOOP';
+
+var isServer  = true;
+
+var validateParams = function(params) {
+  return (
+  (params.private_key || params.hasher_url) &&
+  params.method &&
+  params.path &&
+  params.headers &&
+  (params.signed_headers && params.signed_headers.length > 0) &&
+  params.headers['x-dropoff-date']);
+};
+
+var doHmac = function(message, key) {
+  var hash = CryptoJS.HmacSHA512(message, key);
+  return hash.toString(CryptoJS.enc.Hex);
+};
+
+module.exports.generateXDropoffDate = function() {
+  var timestamp = moment();
+  var x_dropoff_date = timestamp.utc().format('YYYYMMDDTHHmmss') + 'Z';
+  var x_dropoff_date_key = timestamp.utc().format('YYYYMMDD');
+  return {
+    x_dropoff_date : x_dropoff_date,
+    x_dropoff_date_key : x_dropoff_date_key
+  };
+};
+
+module.exports.createHash = function(params) {
+  var canonical_string    = params.canonical_string;
+  var private_key         = params.private_key;
+  var resource            = params.resource;
+  var x_dropoff_date      = params.x_dropoff_date;
+  var x_dropoff_date_key  = params.x_dropoff_date_key;
+
+  var string_to_sign = 'HMAC-SHA512\n' + x_dropoff_date + '\n' + resource + '\n' + doHmac(canonical_string, private_key);
+  var intermediate_key = doHmac(x_dropoff_date_key, 'dropoff' + private_key);
+  intermediate_key = doHmac(resource, intermediate_key);
+  return doHmac(string_to_sign, intermediate_key);
+};
+
+module.exports.sign = function(params, callback) {
+  var signature;
+  var index                 = 0;
+  var canonical_request     = isServer ? new Buffer(8192) : '';
+  var resource              = void(0);
+  var x_dropoff_date        = void(0);
+  var x_dropoff_date_key    = void(0);
+
+  var doWrite = function(data) {
+    if (isServer) {
+      index = index + canonical_request.write(data, index, 'utf-8');
+    } else {
+      canonical_request += data;
+      index = canonical_request.length;
+    }
+  };
+
+  var canonicalRequestToString = function() {
+    if (isServer) {
+      return canonical_request.toString('utf-8', 0, index);
+    } else {
+      return canonical_request;
+    }
+  };
+
+  var doValidateParams = function(cb) {
+    if (validateParams(params)) {
+      x_dropoff_date = params.headers['x-dropoff-date'];
+      x_dropoff_date_key = x_dropoff_date.substring(0, 8);
+      params.headers = _.transform(params.headers, function(result, value, key) {
+        result[key.toLowerCase()] = value;
+      });
+      params.signed_headers = _.transform(params.signed_headers, function(result, value) {
+        result.push(value.toLowerCase());
+      });
+      cb(NO_ERROR, DONE);
+    } else {
+      cb(new Error('Expected private_key, method, url, headers, and signed_headers in params.  query is optional.'));
+    }
+  };
+
+  var writeMethodAndPathToBuffer = function(cb) {
+    doWrite(params.method.toUpperCase());
+    doWrite('\n', index, 'utf-8');
+    var path = params.path;
+    if (path && path.indexOf('?') !== -1) {
+      path = path.substring(0,path.indexOf('?'));
+    }
+    doWrite((path ? path : '/'));
+    doWrite('\n');
+
+    if (path && path.split('/').length > 1) {
+      resource = path.split('/')[1];
+    }
+
+    if (resource) {
+      cb(NO_ERROR, DONE);
+    } else {
+      cb(new Error('No resource found in url ' + params.url));
+    }
+  };
+
+  var writeQueryToBuffer = function(cb) {
+    if (params.query) {
+      var keys = _.keys(params.query).sort();
+      var index = 0;
+      _.forEach(keys, function(key) {
+        doWrite((key + '=' + encodeURIComponent(params.query[key])));
+        index++;
+        if (index !== keys.length) {
+          doWrite('&');
+        }
+        doWrite('\n');
+      });
+    }
+    doWrite('\n');
+    cb(NO_ERROR, DONE);
+  };
+
+  var writeHeaderToBuffer = function(cb) {
+    var header_keys = void(0);
+    if (params.headers && params.signed_headers) {
+      header_keys = '';
+      var keys = params.signed_headers.sort();
+      var indx = 0;
+      _.forEach(keys, function(key) {
+        header_keys += key.toLowerCase();
+        doWrite((key.toLowerCase() + ':' + params.headers[key].trim()));
+        indx++;
+        if (indx !== keys.length) {
+          header_keys += ';';
+        }
+        doWrite('\n');
+      });
+    }
+    doWrite('\n');
+    if (header_keys) {
+      doWrite(header_keys);
+    }
+    doWrite('\n');
+    cb(NO_ERROR, DONE);
+  };
+
+  var generateSignature = function(cb) {
+    var signature_params = {
+      canonical_string : canonicalRequestToString(),
+      resource : resource,
+      x_dropoff_date : x_dropoff_date,
+      x_dropoff_date_key : x_dropoff_date_key
+    };
+
+    if (params.private_key) {
+      signature_params.private_key = params.private_key;
+      cb(NO_ERROR, signature = module.exports.createHash(signature_params));
+    } else if (params.hasher_url) {
+      request
+        .post(params.hasher_url)
+        .send(signature_params)
+        .end(function(error, response) {
+          if (error) {
+            cb(error);
+          } if (response && response.status === 200 && response.body && response.body.signature) {
+            cb(NO_ERROR, response.body.signature);
+          } else {
+            cb(new Error('Could not generate signature with url: ' + params.hasher_url));
+          }
+        });
+    } else {
+      cb(new Error('Not enough data to create proper signature'));
+    }
+  };
+
+  var seriesHandler = function (error, results) {
+    if (params.return_as_auth_header && params.public_key) {
+      signature = 'Authorization: HMAC-SHA512 Credential=' + params.public_key + ',SignedHeaders=' + params.signed_headers.join(';') + ',Signature=' + signature;
+    }
+    callback(error, signature);
+  };
+
+  async.series({
+    validate : doValidateParams,
+    method_and_path : writeMethodAndPathToBuffer,
+    query : writeQueryToBuffer,
+    header : writeHeaderToBuffer,
+    generate : generateSignature
+  }, seriesHandler);
+};
+
