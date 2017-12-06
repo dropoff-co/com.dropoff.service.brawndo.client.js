118b33884f023f9a79ec018a7b58b98d022cbe9c
Algis Woss <awoss@dropoff.com>
2017-12-06 08:52:29 -0600

    Cleaned up other clients

--- clients/go/src/dropoff.com/brawndo/SimulateResponse.go
+++
@@ -1,8 +0,0 @@
-package brawndo
-
-type SimulateOrderResponse struct {
-	OrderId			string 	`json:"order_id"`
-	OrderDetailsUrl		string 	`json:"order_details_url"`
-	Timestamp 		string 	`json:"timestamp"`
-	Success			bool	`json:"success"`
-}
